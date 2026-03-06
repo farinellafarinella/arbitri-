0919391d0a6ee41fbdc27803aa262a3996fa0dd4
farinellafarinella <mrpinkukulele@gmail.com>
2026-03-06 10:37:26 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,11 +13,6 @@ const ASSETS = [
   "coin.js",
   "tournaments.js",
   "firebase-config.js",
-  "fcm-config.js",
-  "fcm.js",
-  "admin-notify.js",
-  "referee.html",
-  "referee.js",
   "manifest.json",
   "icon.png"
 ];
