17876d62da90b86a7826c3f76593fb67f5ba0068
farinellafarinella <mrpinkukulele@gmail.com>
2026-03-18 10:37:35 +0100

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -1,5 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("sw.js").catch(() => {});
+    navigator.serviceWorker.register("/sw.js", { scope: "/" }).then((registration) => {
+      registration.update().catch(() => {});
+    }).catch(() => {});
   });
 }
