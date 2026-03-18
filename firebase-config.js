ecdb0ddf904804c21a41945022e0f7d18144d179
farinellafarinella <mrpinkukulele@gmail.com>
2026-03-18 10:24:33 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -12,4 +12,4 @@ window.ADMIN_EMAILS = [
   "inserisci-admin@example.com"
 ];
 
-window.FCM_WEB_VAPID_KEY = BJZJlIzE_9GY4p_b1IvaevsSSUCfnyRtGhpzhm38bS9mM5OOugLduOUi1IkRfoIPhoHxt01eeWN2BaSHFypMgPE;
+window.FCM_WEB_VAPID_KEY = "BJZJlIzE_9GY4p_b1IvaevsSSUCfnyRtGhpzhm38bS9mM5OOugLduOUi1IkRfoIPhoHxt01eeWN2BaSHFypMgPE";
