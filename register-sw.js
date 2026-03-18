ed7303b5a9b11d528ec26fd923fb5033824e4ffc
farinellafarinella <mrpinkukulele@gmail.com>
2026-03-18 11:24:28 +0100

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -1,7 +1,8 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
+    const version = "20260318b";
     const basePath = window.location.pathname.replace(/\/[^/]*$/, "/");
-    const serviceWorkerUrl = `${basePath}sw.js`;
+    const serviceWorkerUrl = `${basePath}sw.js?v=${version}`;
     navigator.serviceWorker.register(serviceWorkerUrl, { scope: basePath }).then((registration) => {
       registration.update().catch(() => {});
     }).catch(() => {});
